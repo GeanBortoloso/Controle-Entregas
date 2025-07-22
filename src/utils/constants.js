4869d448c6beda3015694ff3acde776c79f061cd
GeanBortoloso <geanaugustobortoloso@gmail.com>
2025-07-22 14:50:13 -0400

    Configura a Versão Teste e remove informações sensíveis

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,23 +1,19 @@
 export const DRIVER_VEHICLE_MAPPING = [
-    { driver: "Lucas", defaultVehicle: "FAN 160 (QCV-4G27)" },
-    { driver: "Vagner", defaultVehicle: "FAN 160 (QCV-4G37)" },
-    { driver: "Ueriqui", defaultVehicle: "FAN 160 (QCI-0679)" },
-    { driver: "Natanael", defaultVehicle: "FAN 125 (QBX -2312)" },
-    { driver: "Flavio", defaultVehicle: " " },
-    { driver: "Vyctor", defaultVehicle: "FAN 160 (RAL-3G00)" },
+    { driver: "Name1", defaultVehicle: "Vehicle1" },
+    { driver: "Name2", defaultVehicle: "Vehicle2" },
+    { driver: "Name3", defaultVehicle: "Vehicle3" },
+    { driver: "Name4", defaultVehicle: "Vehicle4" },
+    { driver: "Name5", defaultVehicle: "Vehicle5" },
+    { driver: "Name6", defaultVehicle: "Vehicle6" },
 ];
 
 export const ALL_VEHICLES = [
-    "MONTANA (SPD-2G30)", 
-    "MONTANA (SPD-2G90)", 
-    "STRADA (BCJ-1F38)", 
-    "STRADA (RRY-9F54)", 
-    "STRADA (QBV-4372)", 
-    "FAN 160 (QCV-4G37)", 
-    "FAN 160 (QCI-0679)", 
-    "FAN 160 (QCV-4G27)", 
-    "FAN 160 (RAL-3G00)", 
-    "FAN 125 (QBX -2312)"
+    "Vehicle1", 
+    "Vehicle2", 
+    "Vehicle3", 
+    "Vehicle4", 
+    "Vehicle5", 
+    "Vehicle6", 
 ];
 
 export const PASSWORD = "0408"; // Senha para o arquivo secreto
